9954cb81905447eaca0d878484ab122f01297dfe
Huy Pham <cocwedc@gmail.com>
2019-02-08 10:10:21 -0600

    provider

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { createStore } from 'redux';
 import App from './components/App';
 import reducers from './reducers';
 
+//Provider seems wierd, need to dig deeper
+
 ReactDOM.render(
     <Provider store={createStore(reducers)}>
         <App />
