3bb30a3a14e679c6995bd924f87c55180662fe28
huynet <cocwedc@gmail.com>
2019-07-09 12:16:19 -0500

    refactor

--- src/middlewares/logger.js
+++ src/middlewares/logger.js
@@ -1,13 +1,11 @@
-function logger({ getState }) {
-  return next => action => {
-    console.log("*-*-*-*")
-    console.log("will dispatch", action)
-    console.log("state before dispatch", getState())
-    const returnedValue = next(action)
-    console.log("state after dispatch: ", getState())
-    console.log("-*-*-*-")
-    return returnedValue;
-  }
+const logger = ({ getState }) => next => action => {
+  console.log("*-*-*-*")
+  console.log("will dispatch", action)
+  console.log("state before dispatch", getState())
+  const returnedValue = next(action)
+  console.log("state after dispatch: ", getState())
+  console.log("-*-*-*-")
+  return returnedValue;
 }
 
 export default logger;
